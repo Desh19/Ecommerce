baea90e1d0583652a446bbdc736c4043749581f4
SManeesha99 <it20649012@my.sliit.lk>
2024-04-23 14:42:35 +0530

    user service

---
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/services/UserService.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerce.services;
+
+import com.ecommerce.ecommerce.model.User;
+import com.ecommerce.ecommerce.repository.UserRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
